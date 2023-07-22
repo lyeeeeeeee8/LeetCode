5d88c10721720f9152cd645e3e135af2b9f7109d
Yi-En Li <87905682+lyeeeeeeee8@users.noreply.github.com>
2023-07-22 12:58:10 +0800

    Time: 20 ms (45.44%), Space: 19.7 MB (68.70%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int trap(vector<int>& height) 
     {
-        int tallest = 0, tall, res = 0, i;
-        for(int i=0; i<height.size(); i++)
-            if(height[i] > height[tallest]) 
-                tallest = i;
-        for(i=0,tall=0; i<tallest; i++)
+        int res = 0, L, R, tall_L = 0, tall_R = 0;
+        for(L=0,R=height.size()-1; L<R;)
         {
-            if(height[i] > tall) 
-                tall = height[i];
-            else
-                res += tall - height[i];
-        }
-        for(i=height.size()-1,tall=0; i>tallest; i--)
-        {
-            if(height[i] > tall) 
-                tall = height[i];
-            else
-                res += tall - height[i];
+            if(height[L] < height[R]) //R is higher, accumulate from L
+            {
+                tall_L = max(tall_L, height[L]);
+                res += tall_L - height[L];
+                L++;
+            }
+            else //L is higher, accumulate from R
+            {
+                tall_R = max(tall_R, height[R]);
+                res += tall_R - height[R];
+                R--;
+            }
         }
         return res;
     }
